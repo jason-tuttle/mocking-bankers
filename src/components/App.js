037d489692fccf3c12074de6cd19b3a7e8044f5d
Jason Tuttle <jason@hearspotbark.com>
2017-08-05 17:00:39 -0400

    styles applied

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import '../styles/App.css';
 
 
+
 //import components and containers
 
 
@@ -10,12 +11,16 @@ class App extends Component {
   render() {
     return (
 
-        <div className="main">
-          <div className= "jumbotron">
-            <h1 className= "display-3">BankShot</h1>
-            <p className= "lead">Your world wide banking leader.</p>
+        <div style={{fontFamily: 'serif'}}>
+          <div className="col-md-6" style={{backgroundColor: "rgba(200, 200, 200, 0.8)", padding: '50px'}}>
+            <h3>Like the Medicis, or the Rothschilds &mdash; only 21st century!</h3>
             <hr className= "my-4"/>
-            <p>Quick, fast, and sometimes accurate account results.</p>
+            <p style={{textAlign: 'justify'}}>The word bank was borrowed in Middle English from Middle French
+              <em>banque</em>, from Old Italian <em>banca</em>, meaning "table",
+              from Old High German <em>banc</em>, bank "bench, counter". Benches
+              were used as makeshift desks or exchange counters during the
+              Renaissance by Jewish Florentine bankers, who used to make their
+              transactions atop desks covered by green tablecloths.</p>
             <p className= "lead">
               <Link className= "btn btn-primary btn-lg" to="/users" role="button">Check Users</Link>
               </p>
--- src/components/BaseLayout.js
+++ src/components/BaseLayout.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
+import bankImg from  '../images/bank-building.jpg';
 
 class BaseLayout extends Component {
 
 
   render() {
     return (
-      <div>
+      <div style={{
+          backgroundImage: "url('https://media.coindesk.com/uploads/2015/08/bank-building.jpg')",
+          backgroundSize: 'cover',
+          backgroundPosition: 'bottom'
+        }}>
         <header>
           <h1 className="header title">First ReactBank <small>of InterWebs</small></h1>
           <h3 className="header subtitle">Totally secure.</h3>
         </header>
+        <div style={{height: 'calc(100vh - 125px)'}}>
         {this.props.children}
+        </div>
       </div>
     );
   }
